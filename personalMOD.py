0a56714d5b9bbdd76a14b4f3737e716c505ee675
AlejandroGuerra1823 <100006307+AlejandroGuerra1823@users.noreply.github.com>
2023-05-17 18:15:17 -0500

    Se avanza en el archivo PersonalMOD

--- personalMOD.py
+++ personalMOD.py
@@ -1,11 +1,13 @@
 import pandas as pd
 
 RAIZ = 'C:/Dev/test/Trabajo-final-analisis-de-datos'
-ruta_datos_procesados = '/Business/'
+ruta_datos_procesados = './Business/'
 
 empleados_salario = pd.read_csv('./carpetaAux/BDEmpleados.csv')
 
 empleados = pd.read_csv('./carpetaAux/BDEmpleados.csv')
+
+PrestamosHoras = pd.read_csv('./carpetaAux/PrestamosHoras.csv')
 
 #Se retiran los puntos del dataFrame
 empleados['Salario'] = empleados['Salario'].str.replace('.', '')
@@ -41,11 +43,31 @@ empleados['Salario Total'] = empleados['Salario'] + empleados['AuxTPTE']
 
 empleados['Valor Hora']= empleados['Salario Total'] /240
 
-empleados.to_csv(RAIZ + ruta_datos_procesados + 'empleados.csv', index=False)
+empleados.to_csv( ruta_datos_procesados + 'empleados.csv', index=False)
 
 print('-----------------------------')
 
 
-salario_Gasto = empleados_salario['Cargo']
+# salario_Gasto = empleados_salario['Cargo']
 
-salario_Gasto['CodDependencia'] = empleados_salario['CodDependencia']
+# salario_Gasto['CodDependencia'] = empleados_salario['CodDependencia']
+
+PrestamosHoras['Horas'] = PrestamosHoras['Horas'].str.replace(',', '.').astype(float)
+
+nuevo_dataframe = empleados.groupby(['Cargo','CodDependencia'])['Salario Total'].sum().reset_index(name='TotalSalarios')
+
+horasTotal = PrestamosHoras.groupby(['Cargo'])['Horas'].sum().reset_index(name='Horas Prestadas')
+
+nuevo_dataframe = nuevo_dataframe.merge(horasTotal, on='Cargo', how='left')
+
+nuevo_dataframe = nuevo_dataframe.fillna(0)
+
+nuevo_dataframe = nuevo_dataframe.merge(empleados[['Cargo', 'Valor Hora']].drop_duplicates(), on='Cargo', how='left')
+
+nuevo_dataframe['Valor H'] = nuevo_dataframe['Valor Hora'] * nuevo_dataframe['Horas Prestadas']
+
+nuevo_dataframe = nuevo_dataframe.drop(['Valor Hora'], axis=1)
+
+nuevo_dataframe['Salario Gasto'] = PrestamosHoras['DependenciaDestino']
+
+print(nuevo_dataframe)
